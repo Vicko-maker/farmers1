c243ee46ba65b48e11ca217a08c18c8440bc509e
Vicko-maker <korirkelvinkipkirui@gmail.com>
2023-03-24 11:58:43 +0300

    first commit

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbName = "agroculture";
+
+    $conn = mysqli_connect($serverName, $userName, $password, $dbName);
+    if (!$conn)
+    {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    echo 'Successfully Connected';
+
+
+
+    
+
+?>
